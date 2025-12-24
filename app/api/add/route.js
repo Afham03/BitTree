74f9d43647f7006b604ec195ff51151d1b6a9fe9
Muhammad Afham <afhamiqbal@Afhams-MacBook-Pro.local>
2025-12-24 17:21:46 +0500

    Fix mongoes altas

--- app/api/add/route.js
+++ app/api/add/route.js
@@ -1,31 +1,21 @@
+import clientPromise from "@/lib/mongodb"
+
+
 export async function POST(request) {
-  try {
-    const body = await request.json();
+    const body = await request.json()
 
     const client = await clientPromise;
-    const db = client.db("bittree");
-    const collection = db.collection("links");
+    const db = client.db("bittree")
+    const collection = db.collection("links")
 
-    const exists = await collection.findOne({ handle: body.handle });
+    // If the handle is already claimed, you cannot create the bittree
+    const doc = await collection.findOne({handle: body.handle})
 
-    if (exists) {
-      return Response.json({
-        success: false,
-        message: "This Bittree already exists!",
-      });
+    if (doc){
+      return Response.json({ success: false, error: true, message: 'This Bittree already exists!', result: null })
     }
 
-    await collection.insertOne(body);
-
-    return Response.json({
-      success: true,
-      message: "Your Bittree has been generated!",
-    });
-  } catch (err) {
-    console.error("API ERROR:", err);
-    return Response.json(
-      { success: false, message: "Server error" },
-      { status: 500 }
-    );
+    const result = await collection.insertOne(body)
+     
+    return Response.json({ success: true, error: false, message: 'Your Bittree has been generated!', result: result,  })
   }
-}
